4a837efa60c2ecd74f04f8451fd26286913a0488
Elijah Schiltz-Rouse <elijahforest101@gmail.com>
2021-04-01 20:03:14 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards(name,sup) {
+    const a = [];
+    for (let i = 0; i < name.length; i++){
+        a.push(`Thank you, ${name[i]}, for the wonderful ${sup} gift!`);
+    }
+    return a;
+}
+
+function countDown(num){
+    let i = 0;
+    while(i<=num){
+        var dis = num-i;
+        console.log(dis);
+        i++; 
+    }
+}
