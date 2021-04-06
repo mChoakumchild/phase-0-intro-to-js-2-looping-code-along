c93b493441df0269f3b04a7174672276ad6da26d
Elijah Schiltz-Rouse <elijahforest101@gmail.com>
2021-04-05 22:51:07 -0400

    commit tree 4925612fe2edd4652d94a313fe283404d861ddf9

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 
-function writeCards(name,sup) {
-    const a = [];
-    for (let i = 0; i < name.length; i++){
-        a.push(`Thank you, ${name[i]}, for the wonderful ${sup} gift!`);
-    }
-    return a;
-}
+//function writeCards(name,sup) {
+ //   const a = [];
+  //  for (let i = 0; i < name.length; i++){
+ //       a.push(`Thank you, ${name[i]}, for the wonderful ${sup} gift!`);
+//    }
+ //   return a;
+//}
 
 function countDown(num){
     let i = 0;
     while(i<=num){
-        var dis = num-i;
+        let dis = num-i;
         console.log(dis);
         i++; 
     }
+}
+
+
+
+
+function writeCards(a,b) {
+const c = [];
+    for( let i = 0; i < a.length; i++) {
+        c.push(`Thank you, ${a[i]}, for the wonderful ${b} gift!`)
+    }
+return c
+
 }
